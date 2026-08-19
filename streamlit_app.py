a998107c27492d0ee9254c8841d92b0fe8969c69
SandyCheeks <165305090+hsandorf@users.noreply.github.com>
2026-08-18 20:42:46 -0400

    Refactor Streamlit app configuration and navigation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,12 @@
-import pandas as pd
-import plotly.express as px
 import streamlit as st
 
-st.title("Streamlit Google Drive Parquet Streamer")
+st.set_page_config(page_title="DeKalb ALPR Data Search", page_icon="👮", layout="wide")
 
+pages = [
+    st.Page("app_pages/overview.py", title="About Automated License Plate Readers (Flock Cameras)", icon="❓"),
+    st.Page("app_pages/dekalb_pd_audit.py", title="DeKalb PD Flock ALPR Use", icon="🚓"),
+    st.Page("app_pages/officer_lookup.py", title="Who Else is Searching DeKalb's Data?", icon="🔍"),
+]
 
-import pandas as pd
-import streamlit as st
-
-DATA_URL = "https://www.dropbox.com/scl/fi/oqk2y6nnp6vcxrf67hwy6/1_25-5_26-Combined-Network.parquet?rlkey=xul1b4j7hnjhipm6fv6ah3snx&st=1jg7kjqn&dl=1"
-
-#@st.cache_data
-def load_parquet(url: str) -> pd.DataFrame:
-    return pd.read_parquet(url)
-
-df = load_parquet(DATA_URL)
+pg = st.navigation(pages)
+pg.run()

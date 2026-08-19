0f766b2419621288d1566ad49730889786e49ba5
SandyCheeks <165305090+hsandorf@users.noreply.github.com>
2026-08-18 21:03:09 -0400

    Update page paths in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.set_page_config(page_title="DeKalb ALPR Data Search", page_icon="👮", layout="wide")
 
 pages = [
-    st.Page("app_pages/about_flock.py", title="About Automated License Plate Readers (Flock Cameras)", icon="❓"),
-    st.Page("app_pages/dekalb_pd.py", title="DeKalb PD Flock ALPR Use", icon="🚓"),
-    st.Page("app_pages/network_audit.py", title="Who Else is Searching DeKalb's Data?", icon="🔍"),
+    st.Page("pages/about_flock.py", title="About Automated License Plate Readers (Flock Cameras)", icon="❓"),
+    st.Page("pages/dekalb_pd.py", title="DeKalb PD Flock ALPR Use", icon="🚓"),
+    st.Page("pages/network_audit.py", title="Who Else is Searching DeKalb's Data?", icon="🔍"),
 ]
 
 pg = st.navigation(pages)

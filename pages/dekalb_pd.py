1bb5a30326e38a2d4d75268318c35732d0bbf3c6
SandyCheeks <165305090+hsandorf@users.noreply.github.com>
2026-08-21 08:08:46 -0400

    Fix formatting issues in dekalb_pd.py documentation

--- pages/dekalb_pd.py
+++ pages/dekalb_pd.py
@@ -22,7 +22,7 @@ st.write(
     This was not an isolated incident, or even unique to DeKalb County; dozens of similar stories have spread across the nation, just search 'Flock misuse' and see tons of examples.
       See the charts below and make your own decisions on how DeKalb has been using YOUR ALPR data. A few highlights:  
       -DeKalb County PD conducted over 158,000 searches in the past 18 months, 27% using a Mobile device  
-      -With each search taking roughly a minute, likely longer, that means 158,000 minutes of searches ALPR images. Over an entire year of working days for one officer.
+      -With each search taking roughly a minute, likely longer, that means 158,000 minutes of searches ALPR images. Over an entire year of working days for one officer.  
       -According to the Federal Crime Data Explorer, DeKalb County has not seen a meaningful increase in crime solve rates, even vehiclular crimes https://cde.ucr.cjis.gov/LATEST/webapp/#/pages/home """
   )
 

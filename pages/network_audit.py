b050d17873e8abbe1d3d5411ce07f0e458cc0bb0
SandyCheeks <165305090+hsandorf@users.noreply.github.com>
2026-08-21 00:38:32 -0400

    Update DATA_URL to point to GitHub release

--- pages/network_audit.py
+++ pages/network_audit.py
@@ -28,7 +28,7 @@ See below to explore Flock data requests from DeKalb cameras yourself!"""
 )
 
 
-DATA_URL = "https://www.dropbox.com/scl/fi/1zirc49rz4uan2e1213sj/1_25-5_26-Combined-Network.parquet?rlkey=ztbpmc82khf7rwjnj4kpsusca&st=w30vsqjd&dl=1"
+DATA_URL = "https://github.com/hsandorf/deflockdekalb/releases/download/deflockdekalb/1_25-5_26.Combined.Network.parquet"
 @st.cache_data
 def load_parquet(url: str) -> pd.DataFrame:
     return pd.read_parquet(url)
